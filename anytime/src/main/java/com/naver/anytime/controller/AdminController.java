79f19fd85039ed6f478864ddd907fe5bde7b35dc
leeyounhee <youni929@nate.com>
2023-10-17 23:10:52 +0900

    scheduled_완성(승인 처리 후 매일마다 update)

--- anytime/src/main/java/com/naver/anytime/controller/AdminController.java
+++ anytime/src/main/java/com/naver/anytime/controller/AdminController.java
@@ -6,17 +6,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.naver.anytime.domain.Board;
 import com.naver.anytime.service.BoardService;
 import com.naver.anytime.service.PostService;
-import com.naver.constants.AnytimeConstants;
 
-@RestController
+@Controller
 //@RequestMapping(value = "/admin")
 public class AdminController {
 
@@ -47,23 +48,21 @@ public class AdminController {
 	}
 
 	@RequestMapping(value = "/updateBoardStatus", method = RequestMethod.POST)
-	public int updateBoardStatus(int board_id, int approvalStatus, String rejectionreason) {
-		logger.info("board_id : " + board_id + "approvalStatus : " + approvalStatus);
-		int result = 0;
-//		 if (approvalStatus != AnytimeConstants.BOARD_APPROVAL_SCHEDULED_FOR_DENIAL) {
-		result = boardService.updateBoardStatus(board_id, approvalStatus);
-//		 }
-//		else {
-//			result = 1;
-//		}
-		logger.info("result : " + result);
-		return result;
+	@ResponseBody
+	public int updateBoardStatus(@RequestParam("board_id") int board_id,
+			@RequestParam("approvalStatus") int approvalStatus,
+			@RequestParam("rejectionreason") String rejectionreason) {
+		return boardService.updateApprovalStatus(board_id, approvalStatus, rejectionreason);
 	}
 
-	@Scheduled(cron = "0 0 0 * * ?")
-	public String updateBoardStatusComplete() {
+	@Scheduled(cron = "0 0 * * * ?" /*매일마다*/) 
+	// "0 0/5 * 1/1 * ?" 5분마다
+	/*
+	 * 0: 초 0/5: 5분 간격 (매 5분) 1/1: 매일 * : 매월 ?: 요일을 지정하지 않음
+	 */
+	public int updateBoardStatusComplete() {
 
-		return "0";
+		return boardService.updateBoardStatusComplete();
 	}
 
 	@RequestMapping(value = "/reportAdmin", method = RequestMethod.GET)
